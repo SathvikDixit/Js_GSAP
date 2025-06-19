e34b37c12272914e33e6fa61713f5bcccf18fa7f
SathvikDixit <sathvikdixit21@gmail.com>
2025-06-19 11:34:21 +0530

    Just completed 1st round

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-gsap.to("#box", {
-    x: 1000,
-    duration: 2,
-    delay: 1,
-    rotate: 360
+// gsap.to("#box", {
+//     x: 1000,
+//     duration: 2,
+//     delay: 1,
+//     rotate: 360,
+//     borderRadius: "50%",
+//     scale: 0.5
+// })
 
-})
+// gsap.from("#box2", {
+//     x: 1000,
+//     duration: 2,
+//     delay: 1,
+//     rotate: 360,
+//     borderRadius: "50%",
+//     scale: 2
+// })
 
-gsap.from("#box2", {
-    x: 1000,
-    duration: 2,
-    delay: 1,
-    rotate: 360
-})
 
