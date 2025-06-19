7c0f15f8af5d60c0e1e453ffa2d2d9c61586a3c4
SathvikDixit <sathvikdixit21@gmail.com>
2025-06-19 20:42:04 +0530

    Just completed GSAP part_1

--- script.js
+++ script.js
@@ -19,23 +19,23 @@
 // })
 
 
-gsap.from("h1", {
-    opacity: 0,
-    duration: 3,
-    y: 45,
-    delay: 1,
-    stagger: 0.3 // It runs each element one at a time if there are lots of same tag elements...
-})
+// gsap.from("h1", {
+//     opacity: 0,
+//     duration: 3,
+//     y: 45,
+//     delay: 1,
+//     stagger: 0.3 // It runs each element one at a time if there are lots of same tag elements...
+// })
 
 
-gsap.to("#dabbi", {
-    x: 1000,
-    duration: 1, 
-    delay: 1,
-    rotate: 360,
-    repeat: -1,  //repeat -1 for infinity loop
-    yoyo: true
-})
+// gsap.to("#dabbi", {
+//     x: 1000,
+//     duration: 1, 
+//     delay: 1,
+//     rotate: 360,
+//     repeat: -1,  //repeat -1 for infinity loop
+//     yoyo: true
+// })
 
 
 // gsap.to("#dabbi1", {
@@ -63,22 +63,43 @@ gsap.to("#dabbi", {
 //     yoyo: true
 // })
 
+// var tl1 = gsap.timeline()
+// tl1.to("#dabbi1", {
+//     x:1000,
+//     duration: 2,
+//     delay: 1, 
+//     rotate: 360,
+
+// })
+// tl1.to("#dabbi2", {
+//     x:1000,
+//     duration: 2,
+//     rotate: 360,
+
+// })
+// tl1.to("#dabbi3", {
+//     x:1000,
+//     duration: 2,
+//     rotate: 360
+// })
+
+
 var tl = gsap.timeline()
-tl.to("#dabbi1", {
-    x:1000,
-    duration: 2,
-    delay: 1,
-    rotate: 360,
-
+tl.from("h2", {
+    y: -20,
+    opacity: 0,
+    duration: 1,
+    delay: 0
 })
-tl.to("#dabbi2", {
-    x:1000,
-    duration: 2,
-    rotate: 360,
-
+tl.from("h4", {
+    y: -20,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.3
 })
-tl.to("#dabbi3", {
-    x:1000,
-    duration: 2,
-    rotate: 360
+tl.from("h1", {
+    y: 20,
+    opacity: 0,
+    duration: 0.5,
+    scale: 0.2
 })
