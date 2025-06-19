9b30cadc9e8044886df50def868916344a8d9da1
SathvikDixit <sathvikdixit21@gmail.com>
2025-06-19 11:56:13 +0530

    Round 2 complete aithu broooo

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
 //     delay: 1,
 //     rotate: 360,
 //     borderRadius: "50%",
-//     scale: 0.5
+//     scale: 0.5,
+//      repeat: -1  // -1 for infinity loop
 // })
 
 // gsap.from("#box2", {
@@ -13,7 +14,8 @@
 //     delay: 1,
 //     rotate: 360,
 //     borderRadius: "50%",
-//     scale: 2
+//     scale: 2,
+//  repeat: 2  // repeat for 2 times.
 // })
 
 
@@ -22,5 +24,15 @@ gsap.from("h1", {
     duration: 3,
     y: 45,
     delay: 1,
-    stagger: 1 // It runs each element one at a time if there are lots of same tag elements...
+    stagger: 0.3 // It runs each element one at a time if there are lots of same tag elements...
+})
+
+
+gsap.to("#dabbi", {
+    x: 1000,
+    duration: 1, 
+    delay: 1,
+    rotate: 360,
+    repeat: -1,  //repeat -1 for infinity loop
+    yoyo: true
 })
