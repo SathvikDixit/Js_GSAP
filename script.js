c82f6732170ac6127a18d0bef87840dd6edf3fa2
SathvikDixit <sathvikdixit21@gmail.com>
2025-06-19 11:22:21 +0530

    qwioeurpiqwe

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 gsap.to("#box", {
     x: 1000,
     duration: 2,
-    delay: 1
+    delay: 1,
+    rotate: 360
+
 })
 
 gsap.from("#box2", {
     x: 1000,
     duration: 2,
-    delay: 1
+    delay: 1,
+    rotate: 360
 })
+
