4896afd1d4ae9cf733e0f3a0085d0f70d9e7640d
SathvikDixit <sathvikdixit21@gmail.com>
2025-06-19 23:55:02 +0530

    asdasdff

--- script.js
+++ script.js
@@ -103,3 +103,25 @@
 //     duration: 0.5,
 //     scale: 0.2
 // })
+
+
+// GSAP  PART-2
+
+
+gsap.from("#page1 #box", {
+    scale: 0,
+    delay: 1,
+    duration: 2,
+    rotate: 360
+})
+
+gsap.from("#page2 #box", {
+    scale: 0,
+    delay: 1,
+    duration: 2,
+    rotate: 360,
+    scrollTrigger: {
+        trigger: "#page2 #box",
+        scroller: "body"
+    }
+})
